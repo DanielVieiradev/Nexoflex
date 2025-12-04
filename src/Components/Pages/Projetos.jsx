8723c55c5f8d9006d7462658fb2fa29bcad0b23f
DanielVieiradev <daniel.viieira.dasilva0@gmail.com>
2025-12-04 11:26:28 -0300

    Correção da pasta Projetos.

--- src/Components/Pages/Projetos.jsx
+++ src/Components/Pages/Projetos.jsx
@@ -3,14 +3,19 @@ import styles from "./Projeto.module.css";
 
 function Projetos() {
   return (
-    <div className={styles.pageContainer}>
-      <h1 className={styles.title}>Meus Projetos</h1>
-      <p className={styles.subtitle}>
-        Aqui você pode visualizar todos os seus projetos cadastrados.
-      </p>
+    <div className={styles.page_wrapper}>
+      <div className={styles.content_wrapper}>
+        <div className={styles.pageContainer}>
+          <h1 className={styles.title}>Meus Projetos</h1>
+          <p className={styles.subtitle}>
+            Aqui você pode visualizar todos os seus projetos cadastrados.
+          </p>
 
-      <div className={styles.gridWrapper}></div>
-      <ProjectsList />
+          <div className={styles.gridWrapper}></div>
+
+          <ProjectsList />
+        </div>
+      </div>
     </div>
   );
 }
